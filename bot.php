679247ed9d47c1b0aef8e8d714071c82052441e4
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2021-02-05 16:27:23 +0100

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,346 +1,100 @@
 <?php
-    date_default_timezone_set("Asia/kolkata");
-    //Data From Webhook
-    $content = file_get_contents("php://input");
-    $update = json_decode($content, true);
-    $chat_id = $update["message"]["chat"]["id"];
-    $message = $update["message"]["text"];
-    $id = $update["message"]["from"]["id"];
-    $username = $update["message"]["from"]["username"];
-    $firstname = $update["message"]["from"]["first_name"];
-    $bot_name = "" ;//your bot name
- /// for broadcasting in Channel
-$channel_id = "-100xxxxxxxxxx"; 
-
-    //Extact match Commands
-    if($message == "/start"){
-        send_message($chat_id, "Hey $firstname I am $bot_name \nSupport Group - @world_bots \nUse /cmds to view commands \nBot developed by @reboot13 ");
-    }
-
-    if($message == "/cmds"){
-        send_message($chat_id, "
-          /search <input> (Google search)
-          \n/syt <query> (Youtube Search)
-          \n/dict <word> (Dicitonary)
-          \n/smirror <name> (Search Movies/Series)
-          \n/bin <bin> (Bin Data)
-          \n/weather <name of your city> (Current weather Status)
-          \n/dice (random 1-6)
-          \n/date (today's date)
-          \n/time (current time)
-          \n/git <username>
-          \n/info (User Info)
-          \n/donate (Donate to Creator)
-          ");
-    }
-
-    if($message == "/dice"){
-        sendDice($chat_id, "🎲");
-    }
-    if($message == "/date"){
-        $date = date("d/m/y");
-        send_message($chat_id, $date);
-    }
-   if($message == "/time"){
-        $time = date("h:i a", time());
-        send_message($chat_id, $time);
-    }
-    
-     if($message == "/info"){
-        send_message($chat_id, "User Info \nName: $firstname\nID:$id \nUsername: @$username");
-    }
-
-if($message == "/help"){
-        send_message($chat_id, "Contact @Reboot13");
-    }
-if($message == "/donate"){
-        send_message($chat_id, "https://reboot13.hashnode.dev/donate");
-    }
-
-///Commands with text
-
-
-    //Google Search
-if (strpos($message, "/search") === 0) {
-        $search = substr($message, 8);
-         $search = preg_replace('/\s+/', '+', $search);
-    if ($search != null) {
-     send_message($chat_id, "https://www.google.com/search?q=".$search);
-    }
-  }
-
-//World Mirror Search
-if (strpos($message, "/smirror") === 0) {
-$smovie = substr($message, 9);
-$smovie = preg_replace('/\s+/', '+', $smovie);
-$murl = "[Results-Go to World Mirror](https://witcher.lalbaake456.workers.dev/0:search?q=$smovie)";
-if ($smovie != null) {
-  send_MDmessage($chat_id, $murl);
-}
-}
-
-//Youtube Search
-if (strpos($message, "/syt") === 0) {
-$syt = substr($message, 5);
-$syt = preg_replace('/\s+/', '+', $syt);
-$yurl = "[Open Youtube](https://www.youtube.com/results?search_query=$syt)";
-if ($syt != null) {
-  send_MDmessage($chat_id, $yurl);
-}
-}
-
-
-///Channel BroadCast
-if (strpos($message, "/broadcast") === 0) {
-$broadcast = substr($message, 11);
-// id == (admins user id)
-if ($id == 1171876903 || $id == 1478297206 || $id == 654455829 || $id == 638178378 ) {
-  send_Cmessage($channel_id, $broadcast);
-}
-}
-
-
-//Bin Lookup
-   if(strpos($message, "/bin") === 0){
-        $bin = substr($message, 5);
-   $curl = curl_init();
-   curl_setopt_array($curl, [
-	CURLOPT_URL => "https://lookup.binlist.net/".$bin,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"authority: lookup.binlist.net",
-		"accept: application/json",
-		"accept-language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
-		"origin: https://binlist.net",
-		"https://binlist.net/",
-		"sec-fetch-dest: empty",
-		"sec-fetch-site: same-site"
-	],
+/*
+سطر 65 خلي توكن البوت.
+سطر 66 خلي ايدي مالتك.
+سطر 67 خلي اسم تخزينك #ملاحظه الاذاعه تدعم نوعين مم تخزين txt او json.
+*/
+function EditMsg($chat_id,$message_id,$text,$parse_mode,$disable_web_page_preview,$reply_markup){
+bot('editMessageText',[
+'chat_id'=>$chat_id,
+'message_id'=>$message_id,
+'text'=>$text,
+'parse_mode'=>$parse_mode,
+'disable_web_page_preview'=>$disable_web_page_preview,
+'reply_markup'=>$reply_markup
 ]);
-
-$result = curl_exec($curl);
-curl_close($curl);
-$data = json_decode($result, true);
-$bank = $data['bank']['name'];
-$country = $data['country']['alpha2'];
-$currency = $data['country']['currency'];
-$emoji = $data['country']['emoji'];
-$scheme = $data['scheme'];
-$Brand = $data['brand'];
-$type = $data['type'];
-  if ($scheme != null) {
-        send_message($chat_id, "
-    Bin: $bin
-Type: $scheme
-Brand : $Brand
-Bank: $bank
-Country: $country $emoji
-Currency: $currency
-Credit/Debit:$type
-Checked By @$username ");
-    }
-else {
-    send_message($chat_id, "Enter Valid BIN");
-}
-   }
-    
-
-    //Wheather API
-if(strpos($message, "/weather") === 0){
-        $location = substr($message, 9);
-   $curl = curl_init();
-   curl_setopt_array($curl, [
-CURLOPT_URL => "http://api.openweathermap.org/data/2.5/weather?q=$location&appid=89ef8a05b6c964f4cab9e2f97f696c81",
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 50,
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	CURLOPT_CUSTOMREQUEST => "GET",
-	CURLOPT_HTTPHEADER => [
-		"Accept: */*",
-        "Accept-Language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
-        "Host: api.openweathermap.org",
-        "sec-fetch-dest: empty",
-		"sec-fetch-site: same-site"
-  ],
+}      
+function SendMsg($chat_id,$text,$parse_mode,$disable_web_page_preview,$reply_markup,$message_id){ //SendMessage
+bot('sendMessage',[
+'chat_id'=>$chat_id,
+'text'=>$text,
+'parse_mode'=>$parse_mode,
+'disable_web_page_preview'=>$disable_web_page_preview,
+'reply_markup'=>$reply_markup,
+'reply_to_message_id'=>$message_id,
 ]);
-
-
-$content = curl_exec($curl);
-curl_close($curl);
-$resp = json_decode($content, true);
-
-$weather = $resp['weather'][0]['main'];
-$description = $resp['weather'][0]['description'];
-$temp = $resp['main']['temp'];
-$humidity = $resp['main']['humidity'];
-$feels_like = $resp['main']['feels_like'];
-$country = $resp['sys']['country'];
-$name = $resp['name'];
-$kelvin = 273;
-$celcius = $temp - $kelvin;
-$feels = $feels_like - $kelvin;
-
-if ($location = $name) {
-        send_message($chat_id, "
-    Weather at $location: $weather
-Status: $description
-Temp : $celcius °C
-Feels Like : $feels °C
-Humidity: $humidity
-Country: $country 
-Checked By @$username ");
 }
-else {
-           send_message($chat_id, "Invalid City");
+function SaveJson($file,$array){  
+return file_put_contents($file, json_encode($array));
 }
-    }
+function broadcast($token, $path){
+$GetFile = file_get_contents($path);
+$ex = explode(".",$path)[1];
+if($ex == "json"){
+$Users = json_decode($GetFile,true);
+}else{
+$Users = array_map('intval', explode("\n", $GetFile));
+}
+$Users = array_filter($Users);
+$Users = array_unique($Users);
+$Users = json_encode($Users);
+$update = json_encode(json_decode(file_get_contents("php://input"),true));    
+$url = "https://api.codar.site/Broadcast/$token/?Users=".$Users."&update=".$update;
+$res = file_get_contents($url);
+return json_decode($res,true);
+}
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$chat_id = $message->chat->id;
+$text = $message->text;
+$chatid = $update->callback_query->message->chat->id;
+$messageid = $update->callback_query->message->message_id;
+$data = $update->callback_query->data;
+$from_id = $message->from->id;
+$name = $message->from->first_name;
+$Data_id = $update->callback_query->from->id;
+$message_id = $message->message_id;
+$json = [
+'Token'=>'1153754099:AAFKWFpecPbqPfM5BuJUUXyu2Mm_1NQdd6Q', // خلي توكنك بدل هاذ توكن .
+'admin'=>'853393439', // خلي ايديك بدل الايدي مالتي.
+'file'=>'Member.txt', //ضع اسم تخزينك هنا ان كان صيغ ملفك txt او json.
+];
 
-///Github User API
-if(strpos($message, "/git") === 0){
-  $git = substr($message, 5);
-   $curl = curl_init();
-   curl_setopt_array($curl, [
-CURLOPT_URL => "https://api.github.com/users/$git",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 50,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => [
-    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Encoding: gzip, deflate, br",
-    "Accept-Language: en-GB,en;q=0.9",
-    "Host: api.github.com",
-    "Sec-Fetch-Dest: document",
-    "Sec-Fetch-Mode: navigate",
-    "Sec-Fetch-Site: none",
-    "Sec-Fetch-User: ?1",
-    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
-  ],
+$ex = explode('.',$json['file']);
+if($ex[1] == "txt"){
+$user_id = explode("\n",file_get_contents($json['file']));
+$count = count($user_id)+1;
+}elseif($ex[1] == "json"){
+$user_id = json_decode(file_get_contents($json['file']),true);
+$count = count($user_id['id'])+1; //ضع اسم المكان الذي تنحفظ فيه الايديات بدل كلمه (id) ان كان تخزينك json.
+}else{
+$count = "عزيزي قم بتأكد من اسم تخزينك 📮.";}
+$Key = json_encode(['inline_keyboard' => [ //keyboard 1 #
+[['text'=>"📮| ارسال للكل.",'callback_data' =>"b"],['text'=>"📌| عدد الاعضاء.",'callback_data' =>"m"]],
+],
 ]);
-
-
-$github = curl_exec($curl);
-curl_close($curl);
-$gresp = json_decode($github, true);
-
-$gusername = $gresp['login'];
-$glink = $gresp['html_url'];
-$gname = $gresp['name'];
-$gcompany = $gresp['company'];
-$blog = $gresp['blog'];
-$gbio = $gresp['bio'];
-$grepo = $gresp['public_repos'];
-$gfollowers = $gresp['followers'];
-$gfollowings = $gresp['following'];
-
-
-if ($gusername) {
-        send_message($chat_id, "
-Name: $gname
-Username: $gusername
-Bio: $gbio
-Followers: $gfollowers
-Following : $gfollowings
-Repositories: $grepo
-Website: $blog
-Company: $gcompany
-Github url: $glink
-Checked By @$username ");
+$Key_Back = json_encode(['inline_keyboard' => [ //keyboard 2 #
+[['text'=>"🔰| رجوع.",'callback_data' =>"Back"]],
+],
+]);
+$Bc = json_decode(file_get_contents("Bc.json"),true);
+$caption_bc = '📌| مرحبا بك عزيزي المطور.'.PHP_EOL.'📦| اليك الاوامر الخاصةه بك.'.PHP_EOL.'🎯| اختر من الكيبورد الذي فل الاسفل.';
+if($text == "/start" and $from_id == $json['admin']){ //Start 
+SendMsg($json['admin'],$caption_bc,"Markdown",True,$Key,$message_id);}
+if($message and $text != "/start" and !$data and isset($Bc['Bc']) == "bc" and $from_id == $json['admin']){
+if(broadcast($json['Token'], $json['file'])['ok'] == true){
+broadcast($json['Token'], $json['file']);
+unlink("Bc.json");
+}elseif(broadcast($json['Token'], $json['file'])['ok'] == false){
+SendMsg($json['admin'],broadcast($json['Token'], $json['file'])['description']."\n"."عذرًا ، لا يمكنك إجراء بث ، حاول الآن بعد ".broadcast($json['Token'], $json['file'])['next_broadcast']." ثانية","markdown",True,$Key_Back,$message_id);
+unlink("Bc.json");
 }
-else {
-           send_message($chat_id, "User Not Found \nInvalid github username checked by @$username");
 }
-    }
-
-///Dicitonary API
-if(strpos($message, "/dict") === 0){	
-        $dict = substr($message, 6);	
-   $curl = curl_init();	
-   curl_setopt_array($curl, [	
-	CURLOPT_URL => "https://api.dictionaryapi.dev/api/v2/entries/en/$dict",	
-	CURLOPT_RETURNTRANSFER => true,	
-	CURLOPT_FOLLOWLOCATION => true,	
-	CURLOPT_ENCODING => "",	
-	CURLOPT_MAXREDIRS => 10,	
-	CURLOPT_TIMEOUT => 30,	
-	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,	
-	CURLOPT_CUSTOMREQUEST => "GET",	
-	CURLOPT_HTTPHEADER => [	
-		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",	
-        "Accept-Language: en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",	
-        "Host: oxforddictionaryapi.herokuapp.com",	
-        "Sec-Fetch-Dest: empty",	
-        "Sec-Fetch-Mode: cors",	
-        "Sec-Fetch-Site: cross-site",	
-        ],	
-]);	
-
-
-  $dictionary = curl_exec($curl);	
-  curl_close($curl);	
-
-$out = json_decode($dictionary, true);	
-$word = $out[0]['word'];	
-$noun= $out[0]['meaning']['noun'][0]['definition'];	
-$verb = $out[0]['meaning']['verb'][0]['definition'];	
-$adjective = $out[0]['meaning']['adjective'][0]['definition'];	
-$adverb = $out[0]['meaning']['adverb'][0]['definition'];	
-$pronoun = $out[0]['meaning']['pronoun'][0]['definition'];	
-
-if ($word = $dict) {	
-    send_message($chat_id, "	
-Word: $word 	
-Noun : $noun	
-Pronoun: $pronoun 	
-Verb : $verb 	
-Adjective: $adjective 	
-Adverb: $adverb 	
-Checked By @$username ");	
-    }	
-    else {	
-        send_message($chat_id, "Invalid Input");	
-    }	
-}	
-
-
-
-
-     ///Send Message (Global)
-    function send_message($chat_id, $message){
-        $apiToken =  "API_TOKEN";
-        $text = urlencode($message);
-        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=$text");
-    }
-    
-//Send Messages with Markdown (Global)
-      function send_MDmessage($chat_id, $message){
-       $apiToken =  "API_TOKEN";
-        $text = urlencode($message);
-        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=$text&parse_mode=Markdown");
-    }
-    
-
-///Send Message to Channel
-      function send_Cmessage($channel_id, $message){
-       $apiToken =  "API_TOKEN";
-        $text = urlencode($message);
-        file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$channel_id&text=$text");
-    }
-
- function sendDice($chat_id, $message){
-       $apiToken =  "API_TOKEN";
-        file_get_contents("https://api.telegram.org/bot$apiToken/sendDice?chat_id=$chat_id&emoji=$message");
-    }
-?>
+if($data == "m" and $Data_id == $json['admin']){
+EditMsg($json['admin'],$messageid,"- *".$count."* 🔰.","Markdown",True,$Key_Back);}
+if($data == "b" and $Data_id == $json['admin']){
+EditMsg($json['admin'],$messageid,"🎬| الاذاعة تدعم جميع انواع الصيغ."."\n"."📱| حسننا الان ارسل احد انواع الصيغ."."\n"."📹| نص ، متحركةه ، ملف ، صوره ، ملصق ، فيديو ، فيديو نوت ، صوتيات ، اغاني.","Markdown",True,$Key_Back);
+$Bc['Bc'] = "bc";SaveJson("Bc.json",$Bc);
+}
+if($data == "Back" and $Data_id == $json['admin']){
+EditMsg($json['admin'],$messageid,"*".str_repeat("-=", 18)."*","Markdown",True,$Key);}
